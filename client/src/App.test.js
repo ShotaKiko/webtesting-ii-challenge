5558cc50f41150d5f3eb593f880f566feded6f18
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-25 22:22:17 -0500

    removed logo/service files added jest-dom to dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
+import 'jest-dom/extend-expect'
 
-it('renders without crashing', () => {
+xit('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should render', () => {
+  render(<App />)
+})
