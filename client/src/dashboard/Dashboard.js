6fb3e85fa264b9d845d2f82d81d14878019548f2
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-27 00:03:18 -0500

    added tests for action buttons on dashboard component

--- client/src/dashboard/Dashboard.js
+++ client/src/dashboard/Dashboard.js
@@ -4,14 +4,25 @@ class Dashboard extends React.Component {
     state = {
         strikes: 0,
         balls: 0,
+        message:""
     }
 
     render(){
         return(
             <>
                 <h2>Dashboard</h2>
+                <div>{this.state.message}</div>
+                <button onClick={this.strike}>Strike</button>
+                <button>Ball</button>
+                <button>Foul</button>
+                <button>Hit</button>
             </>
         )
+    }
+
+    strike = () => {
+      this.setState({ strikes: this.state.strikes + 1 })
+      this.setState({ message:"Strike!!" })
     }
 
 }
--- client/src/dashboard/Dashboard.test.js
+++ client/src/dashboard/Dashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 import 'jest-dom/extend-expect'
 
@@ -7,7 +7,44 @@ import 'jest-dom/extend-expect'
 import Dashboard from './Dashboard.js'
 
 describe('the test suite for Dashboard component', () => {
-    it('should render the component', () => {
+    xit('should render the component', () => {
         render(<Dashboard />)
     })
+
+    it('should render the strike button and change message on click', () => {
+        const { getByText } = render(<Dashboard />)
+        const strike = getByText(/strike/i)
+        
+        fireEvent.click(strike)
+        getByText(/strike!!/i)
+    })
+
+    xit('should render the ball button and change message on click', () => {
+        const { getByText } = render(<Dashboard />)
+        const ball = getByText(/ball/i)
+        
+        fireEvent.click(ball)
+        getByText(/ball!!/i)
+    })
+
+    xit('should render the foul button and change message on click', () => {
+        const { getByText } = render(<Dashboard />)
+        const foul = getByText(/foul/i)
+        
+        fireEvent.click(foul)
+        getByText(/foul ball!!/i)
+    })
+
+    xit('should render the hit button and change message on click', () => {
+        const { getByText } = render(<Dashboard />)
+        const hit = getByText(/hit/i)
+        
+        fireEvent.click(foul)
+        getByText(/hit!!/i)
+    })
+
+
+
+
+
 })
